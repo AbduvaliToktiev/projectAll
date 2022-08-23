fafd43960d51c6fc6880e9b6cc988c84195f40da
Admin <abdutokt2004>
2022-08-23 17:13:25 +0600

    Методы админа для просмотра информации о районах, школах, детских садах

--- src/mainPackage/Rayons.java
+++ src/mainPackage/Rayons.java
@@ -1,6 +1,14 @@
 package mainPackage;
 
-public class Rayons {
+import sql.Connect;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rayons extends Connect {
     private Integer id;
     private String nameRayon;
 
@@ -18,5 +26,29 @@ public class Rayons {
 
     public void setNameRayon(String nameRayon) {
         this.nameRayon = nameRayon;
+    }
+
+    public List<Rayons> getRayonsFromDataBase() {
+        List<Rayons> rayonsList = new ArrayList<>();
+        String sqlRayons = "select * from \"project\".rayons";
+
+        try (PreparedStatement preparedStatement = connection().prepareStatement(sqlRayons);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+             Rayons rayons = new Rayons();
+             rayons.setId(resultSet.getInt("ID"));
+             rayons.setNameRayon(resultSet.getString("NAME_RAYON"));
+             rayonsList.add(rayons);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+        return rayonsList;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + this.id + " \n" +
+                "NAME_RAYON: " + nameRayon ;
     }
 }
